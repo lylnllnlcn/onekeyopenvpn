93389fa4243605fddb2ec9e290c9c62d3a691104
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-06-22 04:25:08 +0800

    Update onekeyopenvpn.sh

--- onekeyopenvpn.sh
+++ onekeyopenvpn.sh
@@ -4,7 +4,11 @@
 yum -y install epel-release
 
 #安装openvpn
-yum -y install openvpn easy-rsa
+yum -y install openvpn-2.4.6-1.el7 easy-rsa-3.0.3-1.el7
 
+#复制easy到openvpn
+cp -r /usr/share/easy-rsa/ /etc/openvpn/easy-rsa
 
+#复制server.conf
+cp /usr/share/doc/openvpn-2.4.6/sample/sample-config-files/server.conf /etc/openvpn/
 
