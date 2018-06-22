450a0b7c42a73aa4d57bb98eb3c5fa98f381e4b3
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-06-22 11:46:02 +0800

    Update onekeyopenvpn.sh

--- onekeyopenvpn.sh
+++ onekeyopenvpn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#适用centos7
+
 #安装epel源
 yum -y install epel-release
 
@@ -7,8 +9,25 @@ yum -y install epel-release
 yum -y install openvpn-2.4.6-1.el7 easy-rsa-3.0.3-1.el7
 
 #复制easy到openvpn
-cp -r /usr/share/easy-rsa/ /etc/openvpn/easy-rsa
+cp -rf /usr/share/easy-rsa/ /etc/openvpn/easy-rsa
 
 #复制server.conf
-cp /usr/share/doc/openvpn-2.4.6/sample/sample-config-files/server.conf /etc/openvpn/
+cp -f /usr/share/doc/openvpn-2.4.6/sample/sample-config-files/server.conf /etc/openvpn/
+
+#复制vars
+cp -f /usr/share/doc/easy-rsa-3.0.3/vars.example /etc/openvpn/easy-rsa/3.0.3/vars
+
+cd /etc/openvpn/easy-rsa/3.0.3/
+
+#创建pki目录
+./easyrsa init-pki
+
+#生成证书
+./easyrsa --batch "--req-cn=gamer" build-ca nopass
+
+#生成服务端证书
+./easyrsa --batch "--req-cn=gamer" gen-req server nopass
+
+
+
 
