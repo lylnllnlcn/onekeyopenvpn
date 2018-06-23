8a5a6629e656e73a6deaad79be358c71338bfaac
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-06-23 10:52:53 +0800

    Update onekeyopenvpn.sh

--- onekeyopenvpn.sh
+++ onekeyopenvpn.sh
@@ -68,6 +68,9 @@ cd /etc/openvpn
 rm -f server.conf
 curl -o server.conf https://raw.githubusercontent.com/yobabyshark/onekeyopenvpn/master/server.conf
 
+#将openvpn客户端文件下载到client
+curl -o /etc/openvpn/client/client.ovpn https://raw.githubusercontent.com/yobabyshark/onekeyopenvpn/master/client.ovpn
+
 #下载udpspeeder和udp2raw （amd64版）
 mkdir /usr/src/udp
 cd /usr/src/udp
